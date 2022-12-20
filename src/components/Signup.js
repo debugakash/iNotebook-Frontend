7919ee22eff7658e08672039a817275cced39c44
Akash Arya <118762199+debugakash@users.noreply.github.com>
2022-12-20 12:22:06 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -9,7 +9,7 @@ const Signup = (props) => {
         e.preventDefault();
         if (credentials.password === credentials.cpassword) {
             const { name, email, password } = credentials;
-            const response = await fetch("http://localhost:5000/api/auth/createuser", {
+            const response = await fetch("https://inotebook.adaptable.app/api/auth/createuser", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
